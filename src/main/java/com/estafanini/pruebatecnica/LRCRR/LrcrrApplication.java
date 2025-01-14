8e94e401ceb8729665132c61ae3a9ecc2acf60a8
Luis-Rayas <luis.rayas7482@alumnos.udg.mx>
2025-01-14 16:49:15 -0600

    Init - Commit inicial

---
+++ src/main/java/com/estafanini/pruebatecnica/LRCRR/LrcrrApplication.java
@@ -0,0 +1,13 @@
+package com.estafanini.pruebatecnica.LRCRR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LrcrrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LrcrrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estafanini/pruebatecnica/LRCRR/LrcrrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estafanini.pruebatecnica.LRCRR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LrcrrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
